04e8d0afe3a71c7a4a5de6875a0e0cd8a116ab9f
MartinSchere <tinschere@gmail.com>
2021-01-28 12:16:45 -0300

    Demo view added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import React from "react"
 import DefaultLayout from "../layouts/default"
 
+import "./styles.scss"
+
 const index = () => {
   return (
-    // <DefaultLayout>
-    <div>Index</div>
-    // </DefaultLayout>
+    <DefaultLayout>
+      <div className="hero">
+        <h1>Esta es una demo</h1>
+        <h3>¡Visitá la página modular de Python! </h3>
+      </div>
+    </DefaultLayout>
   )
 }
 
